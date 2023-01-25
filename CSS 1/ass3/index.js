e6196a228aed5266e20929585256b556f710ec53
“abhishekd.nimbalkar” <abhishekd.nimbalkar@thewitslab.com>
2023-01-25 19:26:13 +0530

    feat: created responsive navbar

---
+++ CSS 1/ass3/index.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySideNav").style.display="block";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySideNav").style.display="none";
+  }
+
+//  if(window.innerWidth>480){
+//     document.getElementById("mySideNav").style.display="block";
+//  }
