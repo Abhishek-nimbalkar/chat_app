bb3260d5d29fc063dfc89aa41408e08b2316e4da
“abhishekd.nimbalkar” <abhishekd.nimbalkar@thewitslab.com>
2023-02-09 19:50:02 +0530

    feat ass3 added

---
+++ DOM/ass3/js/index.js
@@ -0,0 +1,13 @@
+let input=document.getElementById("input");
+
+input.addEventListener("click",increaseWidth);
+
+function increaseWidth(){
+    input.style.width="200px"
+}
+// let list=document.getElementsByClassName("list")[0];
+// let item=document.createElement("p")
+// let items=list.innerHTML="hello";
+// function addItem(){
+//     list.appendChild(items)
+// }
