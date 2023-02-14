faf501661afc5bbcab0c1ed2af257dd28fde722e
“abhishekd.nimbalkar” <abhishekd.nimbalkar@thewitslab.com>
2023-02-14 20:35:01 +0530

    docs: assessment_2 added

---
+++ TypeScript/Assesment-2/index.js
@@ -0,0 +1,7 @@
+var value = Math.random() < 0.5 ? "a" : "b";
+if (value !== "a") {
+    console.log("a");
+}
+else if (value === "b") {
+    console.log("b");
+}
---
+++ TypeScript/Assesment-2/index.ts
@@ -0,0 +1,23 @@
+// let value= Math.random() < 0.5 ? "a" : "b";
+// if (value !== "a") {
+//   console.log("a");
+// }else if (value ==="b") {
+//   console.log("b");
+// }
+
+
+// function greet(person: string, date: Date) {
+//     console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+//   }
+   
+//   greet("Maddison", new Date());
+
+
+// const names = ["Alice", "Bob", "Eve"]; 
+// // Contextual typing for function
+// names.forEach(function (s) {
+//   console.log(s.toUppercase());
+
+let obj={
+    "a":1,
+}
