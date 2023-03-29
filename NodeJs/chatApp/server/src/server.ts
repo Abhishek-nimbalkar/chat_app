a67ee9b6b29a7a1a7bd935f073d11d1436439ad5
“abhishekd.nimbalkar” <abhishekd.nimbalkar@thewitslab.com>
2023-03-29 18:52:15 +0530

    feat: socket connected

---
+++ NodeJs/chatApp/server/src/server.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response } from "express";
+import {Server} from "socket.io";
+import http from "http"
+import cors from "cors"
+
+const Port=5000 || process.env.PORT;
+const app=express();
+// app.use(express.json());
+app.use(cors())
+
+
+
+const httpServer=http.createServer(app);
+const SocketIO=new Server(httpServer,{
+    cors:{
+        origin:"*"
+    }
+})
+
+SocketIO.on('connection',(socket)=>{
+    console.log(`⚡: ${socket.id} user just connected!`);
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected')
+    });
+    
+})
+
+
+app.get("/",(req:Request,res:Response)=>{
+    res.send("Hello World")
+})
+
+httpServer.listen(Port,()=>{
+    console.log("Server has Started Successfully on http://localhost:"+Port);
+    
+})
