5bce3f59832aac53af05e2925d3ba06af5665893
“abhishekd.nimbalkar” <abhishekd.nimbalkar@thewitslab.com>
2023-03-30 18:38:32 +0530

    fix: home component

--- NodeJs/chatApp/client/src/App.tsx
+++ NodeJs/chatApp/client/src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import io,{Socket} from "socket.io-client";
+import Home from 'components/Home/index';
 
  const socket=io("http://localhost:5000");
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home socket={socket} />} />
+        {/* <Route path="/chat" element={<ChatPage socket={socket} />} /> */}
+      </Routes>
+    </Router>
+    
   );
 }
 
---
+++ NodeJs/chatApp/client/src/components/Home/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const Home = ({socket}:any) => {
+    const nav=useNavigate();
+    const [userName,setUserName]=useState('');
+    const handleSubmit=(e:React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        localStorage.setItem("userName",userName);
+        console.log(e.target);
+        
+        nav("/chat");
+    }
+  return (
+    <form className="home__container" onSubmit={handleSubmit}>
+      <h2 className="home__header">Sign in to Open Chat</h2>
+      <label htmlFor="username">Username</label>
+      <input
+        type="text"
+        minLength={6}
+        name="username"
+        id="username"
+        className="username__input"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button className="home__cta">SIGN IN</button>
+    </form>
+  )
+}
+
+export default Home
